c8e63d5b062ee56f9fce20b057ce0fd806e87c64
Mohamed abdelhaleam <mohamedabdelhaleam13@gmail.com>
2024-01-31 20:16:38 +0200

    Add Games Relations

--- app/Models/Game.php
+++ app/Models/Game.php
@@ -9,7 +9,16 @@ class Game extends Model
 {
     use HasFactory;
     protected $table = 'games';
-    protected $fillable = ['name', 'image', 'countservice'];
-    protected $hidden = [];
+    protected $fillable = ['name', 'image', 'countservice', 'category_id'];
+    protected $hidden = ['category_id'];
     public $timestamps = false;
+
+    public function Product()
+    {
+        return $this->hasMany(Product::class, 'game_id', 'id');
+    }
+    public function Category()
+    {
+        return $this->belongsTo(Category::class, 'category_id', 'id');
+    }
 }
