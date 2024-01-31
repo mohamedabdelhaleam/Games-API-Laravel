83c4c8052d3e6117bd99f2603529a1fc68ae6557
Mohamed abdelhaleam <mohamedabdelhaleam13@gmail.com>
2024-01-31 20:16:48 +0200

    Add Product Relations

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -9,7 +9,12 @@ class Product extends Model
 {
     use HasFactory;
     protected $table = 'products';
-    protected $fillable = ['name', 'price', 'min', 'max'];
-    protected $hidden = [];
+    protected $fillable = ['name', 'price', 'min', 'max', 'game_id'];
+    protected $hidden = ['game_id'];
     public $timestamps = false;
+
+    public function Game()
+    {
+        return $this->belongsTo(Game::class, 'game_id', 'id');
+    }
 }
