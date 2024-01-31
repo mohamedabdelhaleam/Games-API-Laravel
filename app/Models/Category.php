16f24a99291b09812665128fe0f59891e5a30592
Mohamed abdelhaleam <mohamedabdelhaleam13@gmail.com>
2024-01-31 20:16:26 +0200

    Add Category Relations

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -9,7 +9,13 @@ class Category extends Model
 {
     use HasFactory;
     protected $table = 'categories';
-    protected $fillable = ['name', 'image'];   // id  title  image
+    protected $fillable = ['name', 'image'];
     protected $hidden = [];
     public $timestamps = false;
+
+
+    public function Games()
+    {
+        return $this->hasMany(Game::class, 'category_id', 'id');
+    }
 }
