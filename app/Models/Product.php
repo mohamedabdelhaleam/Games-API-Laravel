236fe0c65e7a0bdda65a85f44134b7d47329fb98
Mohamed abdelhaleam <mohamedabdelhaleam13@gmail.com>
2024-02-03 21:08:45 +0200

    Create relation between product and order

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -10,11 +10,16 @@ class Product extends Model
     use HasFactory;
     protected $table = 'products';
     protected $fillable = ['name', 'price', 'min', 'max', 'game_id'];
-    protected $hidden = ['game_id'];
+    protected $hidden = ['game_id', 'pivot'];
     public $timestamps = false;
 
     public function Game()
     {
         return $this->belongsTo(Game::class, 'game_id', 'id');
+    }
+
+    public function order()
+    {
+        return $this->belongsTo(Order::class, 'product_id', 'id');
     }
 }
