ad38aa8c0c2d89188788807690d296724d23c932
Mohamed abdelhaleam <mohamedabdelhaleam13@gmail.com>
2024-02-04 21:14:10 +0200

    Create Routes For Admin

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -33,6 +33,10 @@ class RouteServiceProvider extends ServiceProvider
                 ->prefix('api')
                 ->group(base_path('routes/api.php'));
 
+            Route::middleware(['api','check.user'])
+                ->prefix('admin')
+                ->group(base_path('routes/admin.php'));
+
             Route::middleware('web')
                 ->group(base_path('routes/web.php'));
         });
