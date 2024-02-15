cb961b681ac20cfd430809d02dd8917b336debc3
Mohamed abdelhaleam <mohamedabdelhaleam13@gmail.com>
2024-02-15 21:16:59 +0200

    Add File system

--- config/filesystems.php
+++ config/filesystems.php
@@ -43,6 +43,13 @@ return [
             'visibility' => 'public',
             'throw' => false,
         ],
+        'category' => [
+            'driver' => 'local',
+            'root' => storage_path('public/images/category'),
+            'url' => env('APP_URL').'/storage',
+            'visibility' => 'public',
+            'throw' => false,
+        ],
 
         's3' => [
             'driver' => 's3',
